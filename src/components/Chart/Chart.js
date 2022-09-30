a105f84e9b2bfd1bd56031a40bb05516caad57a3
Stefan Petrov <stefspetrov@gmail.com>
2022-09-30 15:27:06 +0300

    add chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,23 @@
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+
+    const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value)
+    const totaMaximum = Math.max(...dataPointsValues)
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.lebel}
+          value={dataPoint.value}
+          maxValue={totaMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
